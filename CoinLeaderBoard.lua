32aa9ec1179554da9dba12c005ddbc4de24592d7
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2025-10-04 15:50:47 -0400

    Add files via upload

---
+++ CoinLeaderBoard.lua
@@ -0,0 +1,15 @@
+local Players = game:GetService("Players")
+
+local function leaderboardSetup(player)
+	local leaderstats = Instance.new("Folder")
+	leaderstats.Name = "leaderstats"
+	leaderstats.Parent = player
+
+	local gold = Instance.new("IntValue")
+	gold.Name = "Gold"
+	gold.Value = 0
+	gold.Parent = leaderstats
+end
+
+-- Connect the "leaderboardSetup()" function to the "PlayerAdded" event
+Players.PlayerAdded:Connect(leaderboardSetup)
